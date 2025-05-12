d1966ebb18dde7c78a47b4842acec0fd05dfd7c9
Nada Abdalrhman <nadaabdalrhman740@gmail.com>
2025-05-12 03:33:11 +0300

    Problem One

---
+++ 3X3 Random Matrix/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _3X3_Random_Matrix;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Problem One
+        // 3x3 Random matrix
+        FillArr(3, 3, 1, 100);
+        FillArr(9, 9, 5, 500);
+
+        Console.ReadKey();
+    }
+    public static Array CreateArr(int row, int column)
+    {
+        return new int[row, column];
+    }
+    public static Random rand = new Random();
+    public static void FillArr(int row, int column, int from, int to)
+    {
+        int[,] arr = (int[,])CreateArr(row, column);
+        for(int i = 0; i < row; i++)
+        {
+            for(int j = 0; j < column; j++)
+            {
+                arr[i, j] = rand.Next(from, (to + 1));
+            }
+        }
+        PrintMatrix($"The following is a {row}x{column} random matrix: ", arr);
+    }
+    public static void PrintMatrix(string text, int[,] arr)
+    {
+        Console.WriteLine($"{text}");
+        for(int i = 0; i < arr.GetLength(0); i++)
+        {
+            for(int j = 0; j < arr.GetLength(1); j++)
+            {
+                Console.Write($"{arr[i, j].ToString().PadRight(6)}");
+            }
+            Console.Write("\n");
+        }
+    }
+}
+
---
+++ Algorithms & Problem Solving Level 3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Algorithms___Problem_Solving_Level_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+
+        Console.ReadKey();
+    }
+}
+
